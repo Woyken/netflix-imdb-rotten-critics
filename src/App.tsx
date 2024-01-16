bff62c5c3dc0663ef1fe4603f23a919d77bebf10
Woyken <13166802+Woyken@users.noreply.github.com>
2024-01-16 15:59:22 +0200

    Render placeholder on cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,64 @@
-import type { Component } from 'solid-js';
+import { createSignal, For, type Component } from "solid-js";
+import { Portal } from "solid-js/web";
 
-import styles from './App.module.css';
+// Preview on card hover, `previewModal--container mini-modal has-smaller-buttons`
+// Card, `title-card`
+// Preview big popup `previewModal--container detail-modal has-smaller-buttons`
+// BIG main view preview `billboard billboard-pane billboard-originals trailer-billboard`
+
+const OnCards = () => {
+  const [cardsList, setCardsList] = createSignal(new Set<Element>());
+  const obs = new MutationObserver((mutationList, observer) => {
+    for (const mutation of mutationList) {
+      if (mutation.type === "childList") {
+        if (mutation.addedNodes) {
+          mutation.addedNodes.forEach((addedNode) => {
+            if (addedNode instanceof HTMLElement) {
+              const cardElements = addedNode.querySelectorAll(".title-card");
+              setCardsList((prev) => new Set([...prev, ...cardElements]));
+            }
+          });
+        }
+        if (mutation.removedNodes) {
+          mutation.removedNodes.forEach((removedNode) => {
+            if (removedNode instanceof HTMLElement) {
+              setCardsList(
+                (prev) =>
+                  new Set([...[...prev].filter((p) => p !== removedNode)])
+              );
+              const cardElements = removedNode.querySelectorAll(".title-card");
+              cardElements.forEach((cardElement) => {
+                setCardsList(
+                  (prev) =>
+                    new Set([...[...prev].filter((p) => p !== cardElement)])
+                );
+              });
+            }
+          });
+        }
+      }
+    }
+  });
+
+  obs.observe(document.body, { childList: true, subtree: true });
+  const cardElements = document.body.querySelectorAll(".title-card");
+  setCardsList((prev) => new Set([...prev, ...cardElements]));
+
+  return (
+    <For each={[...cardsList()]}>
+      {(cardNode) => (
+        <Portal mount={cardNode}>
+          <div>teeest</div>
+        </Portal>
+      )}
+    </For>
+  );
+};
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div>
+      <OnCards />
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
-
-import './index.css';
-import App from './App';
+import { render } from "solid-js/web";
+import App from "./App";
 
 render(
   () => <App />,
   (() => {
-    const app = document.createElement('div');
-    app.style.flex = '1';
+    const app = document.createElement("div");
+    app.style.position = "fixed";
+    app.style.top = "0px";
+    app.style.right = "0px";
     document.body.append(app);
     return app;
-  })(),
+  })()
 );
